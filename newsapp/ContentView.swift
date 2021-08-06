d158b2d8ba38f63c73fb5f22603817a064103f36
Alok <alokme123@gmail.com>
2021-08-06 19:17:17 +0530

    Initial Commit

---
+++ newsapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newsapp
+//
+//  Created by Alok N on 06/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ newsapp/newsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsappApp.swift
+//  newsapp
+//
+//  Created by Alok N on 06/08/21.
+//
+
+import SwiftUI
+
+@main
+struct newsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
